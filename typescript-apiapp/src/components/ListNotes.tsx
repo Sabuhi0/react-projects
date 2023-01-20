61c7aa3d47499dbdcc633f98afea9de05e5e67d2
Sabuhi0 <sabuhiq0@gmail.com>
2023-01-20 21:55:50 +0400

    Add ListNotes component

---
+++ typescript-apiapp/src/components/ListNotes.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Note } from '../note.model';
+import Notes from './Notes';
+
+interface IListNoteProps {
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}
+
+const ListNotes: React.FC<IListNoteProps> = ({ notes, setNotes }) => {
+  const handleDelete = (id: String) => setNotes(notes.filter(note => note.id !== id));
+  return (
+    <>
+      <h2 className='mt-4'>Notes</h2>
+      <div>{notes.map(note => <Notes key={note.id} note={note} handleDelete={handleDelete} />)}</div>
+    </>
+  )
+}
+
+export default ListNotes;
