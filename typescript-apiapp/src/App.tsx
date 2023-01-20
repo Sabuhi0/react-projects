a0074a4a45bbea61b09aadd7323c4602efd326f9
Sabuhi0 <sabuhiq0@gmail.com>
2023-01-20 23:00:16 +0400

    Update App.tsx

--- typescript-apiapp/src/App.tsx
+++ typescript-apiapp/src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import ListNotes from './components/ListNotes';
+import { Note } from './note.model';
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  useEffect(() => { 
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((data) => setNotes(data))
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
-    <div className="">
-      
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <ListNotes notes={notes} setNotes={setNotes} />
+      </div>
+    </>
   );
 }
 
